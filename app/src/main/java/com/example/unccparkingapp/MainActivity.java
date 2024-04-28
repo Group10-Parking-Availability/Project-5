26449cc62cf9b659e887d623ceb87803308b8701
NickGoodwin03 <Nick@goodwin-house.com>
2024-04-28 04:12:23 -0400

    Changed the program to read data from the excel file instead of reading from firebase

--- app/src/main/java/com/example/unccparkingapp/MainActivity.java
+++ app/src/main/java/com/example/unccparkingapp/MainActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // These are required for apache poi to work
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl");
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl");
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl");
+
+
         setContentView(R.layout.activity_main);
 
         setTitle("UNCC Parking Availability");
